OpenSC/OpenSC

custom action skeleton (without actions) - help welcome ...

win32/Make.rules.mak
@@ -10,6 +10,12 @@ WIX_PATH = "C:\Program Files (x86)\Windows Installer XML v3.6"
 WIX_PATH = "C:\Program Files\Windows Installer XML v3.6"
 !ENDIF
 
+!IF "$(BUILD_ON)" == "WIN64"
+WIX_LIB = "C:\Program Files (x86)\Windows Installer XML v3.6\SDK\VS2012\lib\x64"
+!ELSE
+WIX_LIB = "C:\Program Files\Windows Installer XML v3.6\SDK\VS2012\lib\x86"
+!ENDIF
+
 #Include support for Secure Messaging
 SM_DEF = /DENABLE_SM
 

viktorTarasov/OpenSC-SM

win32/Make.rules.mak
@@ -6,9 +6,25 @@ MINIDRIVER_DEF = /DENABLE_MINIDRIVER
 #Build MSI with the Windows Installer XML (WIX) toolkit, requires WIX >= 3.6
 !IF "$(BUILD_ON)" == "WIN64"
 WIX_PATH = "C:\Program Files (x86)\Windows Installer XML v3.6"
+WIX_INCL_DIR = "/IC:\Program Files (x86)\Windows Installer XML v3.6\SDK\VS2010\inc"
+!IF "$(BUILD_FOR)" == "WIN64"
+WIX_LIB = "C:\Program Files (x86)\Windows Installer XML v3.6\SDK\VS2010\lib\x64"
+!ELSE
+WIX_LIB = "C:\Program Files (x86)\Windows Installer XML v3.6\SDK\VS2010\lib\x86"
+!ENDIF
+
 !ELSE
 WIX_PATH = "C:\Program Files\Windows Installer XML v3.6"
+WIX_INCL_DIR = "/IC:\Program Files\Windows Installer XML v3.6\SDK\VS2010\inc"
+!IF "$(BUILD_FOR)" == "WIN64"
+WIX_LIB = "C:\Program Files\Windows Installer XML v3.6\SDK\VS2010\lib\x64"
+!ELSE
+WIX_LIB = "C:\Program Files\Windows Installer XML v3.6\SDK\VS2010\lib\x86"
 !ENDIF
+
+!ENDIF
+
+
 
 #Include support for Secure Messaging
 SM_DEF = /DENABLE_SM
@@ -67,7 +83,7 @@ CNGSDK_INCL_DIR = "/IC:\Program Files\Microsoft CNG Development Kit\Include"
 #  O1 - minimal code size
 CODE_OPTIMIZATION = /O1
 
-ALL_INCLUDES = /I$(TOPDIR)\win32 /I$(TOPDIR)\src $(OPENSSL_INCL_DIR) $(ZLIB_INCL_DIR) $(LIBLTDL_INCL) $(INTTYPES_INCL_DIR) $(CNGSDK_INCL_DIR)
+ALL_INCLUDES = /I$(TOPDIR)\win32 /I$(TOPDIR)\src $(OPENSSL_INCL_DIR) $(ZLIB_INCL_DIR) $(LIBLTDL_INCL) $(INTTYPES_INCL_DIR) $(CNGSDK_INCL_DIR) $(WIX_INCL_DIR)
 
 !IF "$(DEBUG_DEF)" == "/DDEBUG"
 LINKDEBUGFLAGS = /NODEFAULTLIB:LIBCMT /DEBUG