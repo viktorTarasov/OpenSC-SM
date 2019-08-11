e7a835b70fc445cfc9d160d4dab64baf40349a4e
Viktor Tarasov <viktor.tarasov@gmail.com>
2019-08-11 23:59:29 +0200

    add emv-tool

---
+++ src/tools/emv-tool.c
@@ -0,0 +1,472 @@
+/*
+ * emv-tool.c: Tool for accessing EMV smart cards
+ *
+ * Copyright (C) 2001  Juha Yrjölä <juha.yrjola@iki.fi>
+ * Copyright (C) 2019  Victor Tarasov <viktor.tarasov@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include <string.h>
+#include <errno.h>
+#include <ctype.h>
+#include <sys/stat.h>
+
+#include "libopensc/opensc.h"
+#include "libopensc/cardctl.h"
+#include "libopensc/gp.h"
+#include "util.h"
+
+/* type for associations of IDs to names */
+typedef struct _id2str {
+	unsigned int id;
+	const char *str;
+} id2str_t;
+
+static const char *app_name = "emv-tool";
+
+static int	opt_wait = 0;
+static char **	opt_apdus;
+static char	*opt_reader;
+static char *opt_aid = NULL;
+static int	opt_apdu_count = 0;
+static int	verbose = 0;
+
+enum {
+	OPT_SERIAL = 0x100,
+	OPT_SELECT_AID,
+	OPT_RESET
+};
+
+static const struct option options[] = {
+	{ "atr",		0, NULL,		'a' },
+	{ "aid",        1, NULL,    OPT_SELECT_AID },
+	{ "serial",		0, NULL,	OPT_SERIAL  },
+	{ "list-readers",	0, NULL,		'l' },
+	{ "send-apdu",		1, NULL,		's' },
+	{ "reader",		1, NULL,		'r' },
+	{ "reset",		2, NULL,	OPT_RESET   },
+	{ "wait",		0, NULL,		'w' },
+	{ "verbose",		0, NULL,		'v' },
+	{ NULL, 0, NULL, 0 }
+};
+
+static const char *option_help[] = {
+	"Prints the ATR bytes of the card",
+	"Select application (Visa, MasterCard, ...)", 
+    "Prints the card serial number",
+	"Lists readers",
+	"Sends an APDU in format AA:BB:CC:DD:EE:FF...",
+	"Uses reader number <arg> [0]",
+	"Does card reset of type <cold|warm> [cold]",
+	"Wait for a card to be inserted",
+	"Verbose operation. Use several times to enable debug output.",
+};
+
+static sc_context_t *ctx = NULL;
+static sc_card_t *card = NULL;
+
+static int list_readers(void)
+{
+	unsigned int i, rcount = sc_ctx_get_reader_count(ctx);
+
+	if (rcount == 0) {
+		printf("No smart card readers found.\n");
+		return 0;
+	}
+	printf("# Detected readers (%s)\n", ctx->reader_driver->short_name);
+	printf("Nr.  Card  Features  Name\n");
+	for (i = 0; i < rcount; i++) {
+		sc_reader_t *reader = sc_ctx_get_reader(ctx, i);
+		int state = sc_detect_card_presence(reader);
+		printf("%-5d%-6s%-10s%s\n", i, state & SC_READER_CARD_PRESENT ? "Yes":"No",
+		      reader->capabilities & SC_READER_CAP_PIN_PAD ? "PIN pad":"",
+		      reader->name);
+		if (state & SC_READER_CARD_PRESENT && verbose) {
+			struct sc_card *card;
+			int r;
+			char tmp[SC_MAX_ATR_SIZE*3];
+			sc_bin_to_hex(reader->atr.value, reader->atr.len, tmp, sizeof(tmp) - 1, ':');
+
+			if (state & SC_READER_CARD_EXCLUSIVE)
+				printf("     %s [EXCLUSIVE]\n", tmp);
+			else {
+				if ((r = sc_connect_card(reader, &card)) != SC_SUCCESS) {
+					fprintf(stderr, "     failed: %s\n", sc_strerror(r));
+				} else {
+					printf("     %s %s %s\n", tmp, card->name ? card->name : "", state & SC_READER_CARD_INUSE ? "[IN USE]" : "");
+					sc_disconnect_card(card);
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+
+
+#if 0
+/* The AID of the Card Manager defined by Open Platform 2.0.1 specification */
+static const struct sc_aid gp_card_manager = {
+    {0xA0, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00}, 7
+};
+
+/* The AID of the Issuer Security Domain defined by GlobalPlatform 2.3.1 specification. */
+static const struct sc_aid gp_isd_rid = {
+    {0xA0, 0x00, 0x00, 0x01, 0x51, 0x00, 0x00}, 7
+};
+
+static const struct sc_aid visa_app = {
+    {0xA0, 0x00, 0x00, 0x00, 0x03, 0x10, 0x10}, 7
+};
+#endif
+
+// byte[] getProcessingOptions={(byte)0x80,(byte)0xA8,(byte)0x00,(byte)0x00,(byte)0x02,(byte)0x83,(byte)0x00,(byte)0x00};
+// byte[] readRecord={(byte)0x00,(byte)0xB2,(byte)0x02,(byte)0x0C,(byte)0x00};
+
+
+int
+select_aid(struct sc_card *card, const struct sc_aid *aid)
+{
+    struct sc_apdu apdu;
+    int rv;
+	unsigned char rbuf[0x400];
+
+    sc_format_apdu(card, &apdu, SC_APDU_CASE_4_SHORT, 0xA4, 0x04, 0x00);
+    apdu.lc = aid->len;
+    apdu.data = aid->value;
+    apdu.datalen = aid->len;
+    apdu.resp = rbuf;
+    apdu.resplen = sizeof(rbuf);
+    apdu.le = 0x100;
+
+
+    rv = sc_transmit_apdu(card, &apdu);
+
+    if (rv < 0)
+        return rv;
+
+    rv = sc_check_sw(card, apdu.sw1, apdu.sw2);
+    if (rv < 0)
+        return rv;
+
+    return apdu.resplen;
+}
+
+
+int
+getProcessingOptions(struct sc_card *card)
+{
+    struct sc_apdu apdu;
+    int rv;
+	unsigned char rbuf[0x400];
+    unsigned char GetProcessingOptions[] = {0x83, 0x00};
+
+    sc_format_apdu(card, &apdu, SC_APDU_CASE_4_SHORT, 0xA8, 0x00, 0x00);
+    apdu.cla = 0x80;
+    apdu.lc = sizeof(GetProcessingOptions);
+    apdu.data = GetProcessingOptions;
+    apdu.datalen = sizeof(GetProcessingOptions);
+    apdu.resp = rbuf;
+    apdu.resplen = sizeof(rbuf);
+    apdu.le = 0x100;
+
+    rv = sc_transmit_apdu(card, &apdu);
+
+    if (rv < 0)
+        return rv;
+
+    rv = sc_check_sw(card, apdu.sw1, apdu.sw2);
+    if (rv < 0)
+        return rv;
+
+    return apdu.resplen;
+}
+
+
+static int
+send_apdu(void)
+{
+	sc_apdu_t apdu;
+	u8 buf[SC_MAX_EXT_APDU_BUFFER_SIZE],
+	  rbuf[SC_MAX_EXT_APDU_BUFFER_SIZE];
+	size_t len0, r;
+	int c;
+
+    printf("%s +%i: '%s'\n", __FILE__, __LINE__, opt_aid);
+    if (opt_aid)   {
+        struct sc_aid aid;
+
+        aid.len = sizeof(aid.value);
+        if (sc_hex_to_bin(opt_aid, aid.value, &aid.len))   {
+            fprintf(stderr, "Invalid AID value: '%s'\n", opt_aid);
+            return 2;
+        }
+	
+		if (select_aid(card, &aid) < 0)   {
+            fprintf(stderr, "Cannot select application '%s'\n", opt_aid);
+            return 2;
+        }
+	}
+
+	for (c = 0; c < opt_apdu_count; c++) {
+		len0 = sizeof(buf);
+		sc_hex_to_bin(opt_apdus[c], buf, &len0);
+
+		r = sc_bytes2apdu(card->ctx, buf, len0, &apdu);
+		if (r) {
+			fprintf(stderr, "Invalid APDU: %s\n", sc_strerror(r));
+			return 2;
+		}
+
+		apdu.resp = rbuf;
+		apdu.resplen = sizeof(rbuf);
+
+		printf("Sending: ");
+		for (r = 0; r < len0; r++)
+			printf("%02X ", buf[r]);
+		printf("\n");
+		r = sc_lock(card);
+		if (r == SC_SUCCESS)
+			r = sc_transmit_apdu(card, &apdu);
+		sc_unlock(card);
+		if (r) {
+			fprintf(stderr, "APDU transmit failed: %s\n", sc_strerror(r));
+			return 1;
+		}
+		printf("Received (SW1=0x%02X, SW2=0x%02X)%s\n", apdu.sw1, apdu.sw2,
+		      apdu.resplen ? ":" : "");
+		if (apdu.resplen)
+			util_hex_dump_asc(stdout, apdu.resp, apdu.resplen, -1);
+	}
+	return 0;
+}
+
+
+
+static void print_serial(sc_card_t *in_card)
+{
+    unsigned char resp[0x200];
+	int r = 0;
+
+	r = sc_lock(card);
+    if (opt_aid)   {
+        struct sc_aid aid;
+
+        aid.len = sizeof(aid.value);
+        if (sc_hex_to_bin(opt_aid, aid.value, &aid.len))   {
+            fprintf(stderr, "Invalid AID value: '%s'\n", opt_aid);
+            return;
+        }
+	
+		r = select_aid(card, &aid);
+        if (r < 0)   {
+            fprintf(stderr, "Cannot select application '%s'\n", opt_aid);
+            return;
+        }
+	}
+
+    r = getProcessingOptions(card);
+    if (r < 0)   {
+        fprintf(stderr, "Cannot get ProcessingOptions.\n");
+        return;
+    }
+
+    r = sc_read_record(card, 2, resp, sizeof(resp), SC_RECORD_BY_REC_NR | 0x01);
+    sc_unlock(card);
+	
+    if (r < 0)   {
+        fprintf(stderr, "Cannot read record\n");
+        return;
+    }
+
+    printf("Read record:\n");
+    util_hex_dump_asc(stdout, resp, r, -1);
+    return;
+}
+
+static int card_reset(const char *reset_type)
+{
+	int cold_reset;
+	int r;
+
+	if (reset_type && strcmp(reset_type, "cold") &&
+	    strcmp(reset_type, "warm")) {
+		fprintf(stderr, "Invalid reset type: %s\n", reset_type);
+		return 2;
+	}
+
+	cold_reset = !reset_type || strcmp(reset_type, "cold") == 0;
+
+	r = sc_lock(card);
+	if (r == SC_SUCCESS)
+		r = sc_reset(card, cold_reset);
+	sc_unlock(card);
+	if (r) {
+		fprintf(stderr, "sc_reset(%s) failed: %d\n",
+			cold_reset ? "cold" : "warm", r);
+		return 1;
+	}
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int err = 0, r, c, long_optind = 0;
+	int do_list_readers = 0;
+	int do_send_apdu = 0;
+	int do_print_atr = 0;
+	int do_print_serial = 0;
+	int do_reset = 0;
+	int action_count = 0;
+	const char *opt_reset_type = NULL;
+	char **p;
+	sc_context_param_t ctx_param;
+
+	setbuf(stderr, NULL);
+	setbuf(stdout, NULL);
+
+	while (1) {
+		c = getopt_long(argc, argv, "lr:vs:aw", options, &long_optind);
+		if (c == -1)
+			break;
+		if (c == '?')
+			util_print_usage_and_die(app_name, options, option_help, NULL);
+		switch (c) {
+		case 'l':
+			do_list_readers = 1;
+			action_count++;
+			break;
+		case 's':
+			p = (char **) realloc(opt_apdus,
+					(opt_apdu_count + 1) * sizeof(char *));
+			if (!p) {
+				fprintf(stderr, "Not enough memory\n");
+				err = 1;
+				goto end;
+			}
+			opt_apdus = p;
+			opt_apdus[opt_apdu_count] = optarg;
+			do_send_apdu++;
+			if (opt_apdu_count == 0)
+				action_count++;
+			opt_apdu_count++;
+			break;
+		case 'a':
+			do_print_atr = 1;
+			action_count++;
+			break;
+		case 'r':
+			opt_reader = optarg;
+			break;
+		case 'v':
+			verbose++;
+			break;
+		case 'w':
+			opt_wait = 1;
+			break;
+		case OPT_SELECT_AID:
+            printf("%s +%i: '%s'\n", __FILE__, __LINE__, opt_aid);
+			opt_aid = optarg;
+			break;
+		case OPT_SERIAL:
+			do_print_serial = 1;
+			action_count++;
+			break;
+		case OPT_RESET:
+			do_reset = 1;
+			opt_reset_type = optarg;
+			action_count++;
+			break;
+		}
+	}
+	if (action_count == 0)
+		util_print_usage_and_die(app_name, options, option_help, NULL);
+
+	memset(&ctx_param, 0, sizeof(ctx_param));
+	ctx_param.ver      = 0;
+	ctx_param.app_name = app_name;
+
+	r = sc_context_create(&ctx, &ctx_param);
+	if (r) {
+		fprintf(stderr, "Failed to establish context: %s\n", sc_strerror(r));
+		return 1;
+	}
+
+	ctx->flags |= SC_CTX_FLAG_ENABLE_DEFAULT_DRIVER;
+
+	if (verbose > 1) {
+		ctx->debug = verbose;
+		sc_ctx_log_to_file(ctx, "stderr");
+	}
+
+	if (do_list_readers) {
+		if ((err = list_readers()))
+			goto end;
+		action_count--;
+	}
+	if (action_count <= 0)
+		goto end;
+
+	err = util_connect_card_ex(ctx, &card, opt_reader, opt_wait, 0, verbose);
+	if (err)
+		goto end;
+
+
+
+	if (do_print_atr) {
+		if (verbose) {
+			printf("Card ATR:\n");
+			util_hex_dump_asc(stdout, card->atr.value, card->atr.len, -1);
+		} else {
+			char tmp[SC_MAX_ATR_SIZE*3];
+			sc_bin_to_hex(card->atr.value, card->atr.len, tmp, sizeof(tmp) - 1, ':');
+			fprintf(stdout,"%s\n",tmp);
+		}
+		action_count--;
+	}
+	if (do_print_serial) {
+		if (verbose)
+			printf("Card serial number:");
+		print_serial(card);
+		action_count--;
+	}
+	if (do_send_apdu) {
+		if ((err = send_apdu()))
+			goto end;
+		action_count--;
+	}
+
+	if (do_reset) {
+		if ((err = card_reset(opt_reset_type)))
+			goto end;
+		action_count--;
+	}
+end:
+	if (card) {
+		sc_disconnect_card(card);
+	}
+	if (ctx)
+		sc_release_context(ctx);
+	return err;
+}
